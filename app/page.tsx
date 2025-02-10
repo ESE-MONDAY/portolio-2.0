641a80862416a98d2abe4f6caf02ad0580fd8a16
EseMonday <mondayese8@gmail.com>
2025-02-11 00:56:33 +0100

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -20,13 +20,13 @@ export default function Home() {
         <div className="flex gap-6 items-center flex-col sm:flex-row mt-4">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-6 sm:px-8"
-            href="/portfolio"
+            href="/"
           >
             View Portfolio
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-6 sm:px-8"
-            href="/blog"
+            href="/"
           >
             Read My Blog
           </a>
@@ -51,7 +51,7 @@ export default function Home() {
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://github.com/yourhandle"
+          href="https://github.com/ESE-MONDAY"
           target="_blank"
           rel="noopener noreferrer"
         >
